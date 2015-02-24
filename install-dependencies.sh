6654bac382df14717b1607c48de78739cc5645bc
Arnaud <arnaud@impraise.com>
2015-02-24 15:26:36 +0100

    Add circleci default files

---
+++ install-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
+  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t extra-android-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  touch $DEPS
+fi
